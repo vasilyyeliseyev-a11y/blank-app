02a99fe6e3446492994f07d248ed0d4c370f1b14
vasilyyeliseyev-a11y <vasilyyeliseyev@gmail.com>
2026-02-08 21:05:38 +0000

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.markdown("### Элементы отображения — веб-приложение на Python. Урок Streamlit 3.")
+
+
+table = ({"Column 1":[1,2,3,4,5],
+		  "Column 2":[6,7,8,9,10]})
+
+#таблица с помощью streamlit (только отобразить)
+st.table(table)
+st.dataframe(table)
+
+#таблица с помощью pandas (до таго как отобразить можно настроить)
+df = pd.DataFrame(table)
+df = df[df["Column 1"] > 2]  #условия 
+st.dataframe(df)  #отобразить
+
+st.metric(label='Win Speed',
+		  value='70ms', delta='5.7')
+
+st.markdown("Текст с эмодзи! :joy:")  #эмодзи
